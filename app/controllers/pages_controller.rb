026a6a62e9d709401792e819d57f26e671312c36
Juan LF <juan.lopez@quehago.co>
2012-03-06 19:03:04 -0500

    improving layout and 1 list!

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,13 @@
 class PagesController < ApplicationController
   def home
-    @user1 = params[:user1]? params[:user1] : 'dhh'
-    @user2 = params[:user2]? params[:user2] : 'tenderlove'
-    @user3 = params[:user3]? params[:user3] : 'krainboltgreene'
+    @user1 = (params[:user1] and Twitter.user?(params[:user1]))? params[:user1] : 'dhh'
+    @user2 = (params[:user2] and Twitter.user?(params[:user2]))? params[:user2] : 'tenderlove'
+    @user3 = (params[:user3] and Twitter.user?(params[:user3]))? params[:user3] : 'krainboltgreene'
+    
+    tweet_count = 1
+    
+    tweets = Twitter.user_timeline(@user1, :count => tweet_count) #+ Twitter.user_timeline(@user2, :count => tweet_count) + Twitter.user_timeline(@user3, :count => tweet_count)
+    @tweets = tweets.sort_by{|t| t.created_at}.reverse
     
     respond_to do |format|
       format.html
