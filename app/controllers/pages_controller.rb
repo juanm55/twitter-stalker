eabe897afc1fefb1b74523056eb74b2062097be2
Juan LF <juan.lopez@quehago.co>
2012-03-06 19:13:27 -0500

    fighting time in words

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,9 +4,9 @@ class PagesController < ApplicationController
     @user2 = (params[:user2] and Twitter.user?(params[:user2]))? params[:user2] : 'tenderlove'
     @user3 = (params[:user3] and Twitter.user?(params[:user3]))? params[:user3] : 'krainboltgreene'
     
-    tweet_count = 1
+    tweet_count = 2
     
-    tweets = Twitter.user_timeline(@user1, :count => tweet_count) #+ Twitter.user_timeline(@user2, :count => tweet_count) + Twitter.user_timeline(@user3, :count => tweet_count)
+    tweets = Twitter.user_timeline(@user1, :count => tweet_count) + Twitter.user_timeline(@user2, :count => tweet_count) + Twitter.user_timeline(@user3, :count => tweet_count)
     @tweets = tweets.sort_by{|t| t.created_at}.reverse
     
     respond_to do |format|
